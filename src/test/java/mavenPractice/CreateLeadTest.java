3d054aa7d78e0e07906c816b5c0bfce38ce279d9
Sandhyasanjugithub <sandhyarani.mandarapu@gmail.com>
2022-11-16 15:36:50 +0530

    aa

---
+++ src/test/java/mavenPractice/CreateLeadTest.java
@@ -0,0 +1,14 @@
+package mavenPractice;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadTest {
+
+	@Test
+	public void createLead()
+	{
+		System.out.println("lead created successfully");
+
+	}
+
+}
